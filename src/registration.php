fec97852da30b26c62855bc15bbc9987395172b9
Desislava Zhelyazkova <desi_zhelyazkova@abv.bg>
2021-06-09 21:19:27 +0300

    moreRegistrationIdiotisms

--- src/registration.php
+++ src/registration.php
@@ -6,15 +6,15 @@
     $errors = [];
     $response = [];
 
-    if ($_POST) {
+    if (isset($_POST)) {
         $data = json_decode($_POST['data'], true);
 
-        $username = isset($data['username']);
-        $password = isset($data['password']);
-        $confirmPassword = isset($data['confirmPassword']);
-        $email = isset($data['email']);
+       // $username = $data['username'];
+        //$password = $data['password'];
+        //$confirmPassword = $data['confirmPassword'];
+        //$email = $data['email'];
 
-      /*if (!$data['username']) {
+      if (!$data['username']) {
           echo "user empty";
         $errors[] = "Username is required.";
       }
@@ -35,12 +35,10 @@
       } //Not sure if this is the way
       else if(!preg_match("/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/", $data['email'])) {
         $errors[] = "Email is not valid.";
-      }*/
+      }
 
-      validateData($data);
-
-      if ($username && $password && $confirmPassword && $email) {
-        if ($password != $confirmPassword) {
+      if ($data['username'] && $data['password'] && $data['confirmPassword'] && $data['email']) {
+        if ($data['password'] != $data['confirmPassword']) {
           $errors[] = "Password confirmation does not match password.";
         } else {
             // TODO: 
@@ -62,40 +60,6 @@
       $erros[] = "Invalid request.";
     }
 
-    function validateData($data) {
-        if (!$username) {
-            $errors[] = 'Please enter username';
-        } elseif (mb_strlen($name) > 30) {
-            $errors[] = 'Username problem';
-        } else {
-            $response['username'] = $username;
-        }
-
-        if (!$password) {
-            $errors[] = 'Please enter username';
-        } elseif (mb_strlen($name) > 30) {
-            $errors[] = 'Password problem';
-        } else {
-            $response['password'] = $password;
-        }
-
-        if (!$confirmPassword) {
-            $errors[] = 'Please confirmed password';
-        } elseif ($confirmPassword != $password) {
-            $errors[] = 'Confirm password is not the same as the password.';
-        } else {
-            $response['conformPassword'] = $confirmPassword;
-        }
-
-        if (!$email) {
-            $errors[] = 'Please enter email';
-        } elseif (mb_strlen($name) > 30) {
-            $errors[] = 'Too long email';
-        } else {
-            $response['email'] = $email;
-        }
-
-    }
 
     if ($errors) {
         echo "did you get here";
