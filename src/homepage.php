c835ed35d459be5b2090572dd89ddc7bd2236dea
Nikola <nikola.r.totev@gmail.com>
2021-06-11 00:29:58 +0300

    Update homepage.php

--- src/homepage.php
+++ src/homepage.php
@@ -4,6 +4,10 @@ require_once "db.php";
 require_once "book.php";
 require_once "user.php";
 
+session_start();
+
+header('Content-Type: application/json');
+
 $errors = [];
 $response = [];
 
@@ -15,7 +19,8 @@ if(isset($_POST)){
     $query = $db->getTakenBooks(['user_id' => $data['user_id']]);
 
     if ($query["success"]) {
-        $takenBooks = $query['data']->fetch(PDO::FETCH_ASSOC);
+        echo($query);
+        $takenBooks = $query["data"]->fetch(PDO::FETCH_ASSOC);
 
         if ($user) {
             $response = ['success' => true, 'data'=> $takenBooks];
