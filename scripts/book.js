d11407a3d815a0c1cbf0a620662146165f33efb8
Ivaylo-Valentinov <58509466+Ivaylo-Valentinov@users.noreply.github.com>
2021-06-12 22:19:49 +0300

    Open and read a book

---
+++ scripts/book.js
@@ -0,0 +1,13 @@
+(function () {
+    var params = getParams(window.location.href);
+    if (!params.bookId) {
+        window.location = 'homepage.html';
+    }
+
+    var bookObject = document.getElementById('book-object');
+    var bookLink = document.getElementById('book-link');
+
+    bookObject.setAttribute('data', params.bookId); 
+    bookLink.setAttribute('href', params.bookId);
+    bookLink.innerHTML = params.bookId;
+})();
